194ca30c0a75592e001c9c961f1abf0c9867b8c2
scri0x <106587008+scri0x@users.noreply.github.com>
2023-01-26 02:37:16 +0500

    Update urls.py
    replace cat_id on cat_slug

--- mans/man/urls.py
+++ mans/man/urls.py
@@ -4,9 +4,9 @@ from man.views import *
 urlpatterns = [
     path('', index, name='index'),
     path('about/', about, name='about'),
-    path('addpage/', addpage, name='add_page'),
+    path('addpage/', addpage, name='addpage'),
     path('contact/', contact, name='contact'),
     path('login/', login, name='login'),
     path('post/<slug:post_slug>/', show_post, name='post'),
-    path('category/<int:cat_id>/', show_category, name="category"),
+    path('category/<slug:cat_slug>/', show_category, name="category"),
 ]
