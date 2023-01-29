d5af4c22197362ed44f815553b37acc94b8c83b3
scri0x <106587008+scri0x@users.noreply.github.com>
2023-01-30 00:54:45 +0500

    Update models.py

--- mans/man/models.py
+++ mans/man/models.py
@@ -9,7 +9,7 @@ class Man(models.Model):
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото')
     time_create = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     time_update = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
-    published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    published = models.BooleanField(default=True, verbose_name='Опубликовать')
     cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категории')
 
     def __str__(self) :
