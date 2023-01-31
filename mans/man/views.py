4f52c99c92d147d6e84a38735e6546231085f8bb
scri0x <106587008+scri0x@users.noreply.github.com>
2023-01-31 08:12:24 +0500

    replace function on CBS classes

--- mans/man/views.py
+++ mans/man/views.py
@@ -1,42 +1,41 @@
 from django.http import Http404, HttpResponse, HttpResponseNotFound
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
 from man.models import Man, Category
 from man.forms import *
 from man.templatetags.man_tags import *
 
 
-def index(request):
-    posts = Man.objects.all()
-    template = 'man/index.html'
+class ManIndex(ListView):
+    model = Man
+    template_name = "man/index.html"
+    context_object_name = 'posts'
+    extra_context = {'title': 'Главная страница'}
 
-    context = {
-        'title': 'Главная страница',
-        'posts': posts,
-        'cat_selected': 0,
-    }
-    return render(request, template, context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['cat_selected'] = 0
+        return context
+
+    def get_queryset(self):
+        return Man.objects.filter(published=True)
+
 
 
 def about(request):
     return render(request, 'man/about.html')
 
 
+class AddPage(CreateView):
+    form_class = AddPostForm
+    template_name = 'man/addpage.html'
+    success_url = reverse_lazy('index')
 
-def addpage(request):
-    template = 'man/addpage.html'
-    if request.method == 'POST':
-        form = AddPostForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save() # use form.save without try except
-            return redirect('index')
-    else:
-        form = AddPostForm()
-
-    context = {
-        'title': 'Добавление статьи',
-        'form': form,
-    }
-    return render(request, template, context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Добавление статьи'
+        return context
 
 
 def contact(request):
@@ -47,30 +46,31 @@ def login(request):
     return HttpResponse("Войти")
 
 
-def show_post(request, post_slug):
-    post = get_object_or_404(Man, slug=post_slug)
-    template = 'man/post.html'
+class ShowPost(DetailView):
+    model = Man
+    template_name = 'man/post.html'
+    slug_url_kwarg = 'post_slug'
+    context_object_name = 'post'
 
-    context = {
-        'post': post,
-        'title': post.title,
-        'cat_selected': post.cat_id
-    }
-    return render(request, template, context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = context['post'] # name of post in title
+        return context
 
 
+class ManCategory(ListView):
+    model = Man
+    template_name = 'man/index.html'
+    context_object_name = 'posts'
 
-def show_category(request, cat_slug):
-    posts = Man.objects.filter(cat__slug=cat_slug)
-    template = 'man/index.html'
- 
-    context = {
-        'posts': posts,
-        'title': 'Категория',
-        'cat_selected': cat_slug,
-    }
-    return render(request, template, context)
+    def get_queryset(self):
+        return Man.objects.filter(cat__slug=self.kwargs['cat_slug'], published=True)
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Категория - ' + str(context['posts'][0].cat)
+        context['cat_selected'] = context['posts'][0].cat_id
+        return context
 
 
 def page_not_found(requst, exception):
