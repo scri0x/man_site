9374531a54bea4a51d760da69986640bbb97604f
scri0x <106587008+scri0x@users.noreply.github.com>
2023-01-31 08:11:46 +0500

    used CBV urls

--- mans/man/urls.py
+++ mans/man/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from man.views import *
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', ManIndex.as_view(), name='index'),
     path('about/', about, name='about'),
-    path('addpage/', addpage, name='addpage'),
+    path('addpage/', AddPage.as_view(), name='addpage'),
     path('contact/', contact, name='contact'),
     path('login/', login, name='login'),
-    path('post/<slug:post_slug>/', show_post, name='post'),
-    path('category/<slug:cat_slug>/', show_category, name="category"),
+    path('post/<slug:post_slug>/', ShowPost.as_view(), name='post'),
+    path('category/<slug:cat_slug>/', ManCategory.as_view(), name="category"),
 ]
