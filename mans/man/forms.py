77b87ddad22c36964f02aab41b30c2c27d309526
scri0x <106587008+scri0x@users.noreply.github.com>
2023-02-03 04:58:04 +0500

    created registration form

--- mans/man/forms.py
+++ mans/man/forms.py
@@ -1,5 +1,10 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import UserCreationForm  
+from django.contrib.auth.models import User 
+
+
+
 from man.models import Man, Category
 
 
@@ -22,3 +27,15 @@ class AddPostForm(forms.ModelForm):
         if len(title) > 200:
             raise ValidationError('Длина заголовка привышает 200 символов')
         return title
+
+class RegisterUserForm(UserCreationForm):
+    email = forms.EmailField(label="Email", widget=forms.EmailInput(attrs={'class':'form-input'}))
+    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+        
