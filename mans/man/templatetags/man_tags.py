9c4af02d43d522612c1728f2ba5cc8a0acd99f22
scri0x <106587008+scri0x@users.noreply.github.com>
2023-02-03 04:58:38 +0500

    refactor

--- mans/man/templatetags/man_tags.py
+++ mans/man/templatetags/man_tags.py
@@ -25,7 +25,7 @@ def show_menu_items():
     return {'menu': menu}
 
 
-@register.inclusion_tag('man/list_categories.html')
-def show_categories():
-    cats = Category.objects.all()
-    return {'cats': cats}
+# @register.inclusion_tag('man/list_categories.html')
+# def show_categories():
+#     cats = Category.objects.all()
+#     return {'cats': cats}
