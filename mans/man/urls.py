6a29d3a374e83f0ae293671d66e46e8ae1dc7230
scri0x <106587008+scri0x@users.noreply.github.com>
2023-02-03 04:55:48 +0500

    added register url

--- mans/man/urls.py
+++ mans/man/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('about/', about, name='about'),
     path('addpage/', AddPage.as_view(), name='addpage'),
     path('contact/', contact, name='contact'),
+    path("register/", RegisterUser.as_view(), name="register"),
     path('login/', login, name='login'),
     path('post/<slug:post_slug>/', ShowPost.as_view(), name='post'),
     path('category/<slug:cat_slug>/', ManCategory.as_view(), name="category"),
