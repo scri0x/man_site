0f22146d0b5f949ab5cedd54a6a5d65717c37ad1
scri0x <106587008+scri0x@users.noreply.github.com>
2023-02-04 01:08:49 +0500

    added LoginUserForm

--- mans/man/forms.py
+++ mans/man/forms.py
@@ -1,15 +1,13 @@
 from django import forms
 from django.core.exceptions import ValidationError
-from django.contrib.auth.forms import UserCreationForm  
-from django.contrib.auth.models import User 
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
 
-
-
-from man.models import Man, Category
+from .models import Man, Category
 
 
 class AddPostForm(forms.ModelForm):
-    def __init__(self, *args, **kwargs): # replace ----- -> Категория не выбрана
+    def __init__(self, *args, **kwargs):  # replace ----- -> Категория не выбрана
         super().__init__(*args, **kwargs)
         self.fields['cat'].empty_label = 'Категория не выбрана'
 
@@ -18,18 +16,18 @@ class AddPostForm(forms.ModelForm):
         fields = ['title', 'slug', 'content', 'photo', 'published', 'cat']
         widgets = {
             'title': forms.TextInput(attrs={'class': 'some-input'}),
-            'content': forms.Textarea(attrs={'rows': '20', 'cols': '60'}) ,
+            'content': forms.Textarea(attrs={'rows': '20', 'cols': '60'}),
         }
 
-
-    def clean_title(self): # title validation
+    def clean_title(self):  # title validation
         title = self.cleaned_data['title']
         if len(title) > 200:
             raise ValidationError('Длина заголовка привышает 200 символов')
         return title
 
+
 class RegisterUserForm(UserCreationForm):
-    email = forms.EmailField(label="Email", widget=forms.EmailInput(attrs={'class':'form-input'}))
+    email = forms.EmailField(label="Email", widget=forms.EmailInput(attrs={'class': 'form-input'}))
     username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
     password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
     password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
@@ -38,4 +36,7 @@ class RegisterUserForm(UserCreationForm):
         model = User
         fields = ('username', 'email', 'password1', 'password2')
 
-        
+
+class LoginUserForm(AuthenticationForm):
+    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
